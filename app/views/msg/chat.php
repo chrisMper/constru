c040f0ffed3a12a5d41ecf7e8455fb5a3f3a6de9
chrisMper <chrisperera1999@gmail.com>
2023-02-05 13:32:47 +0530

    msg view completed

--- app/views/msg/chat.php
+++ app/views/msg/chat.php
@@ -33,7 +33,46 @@
             <p>This is an incoming message This is an incoming message This is an incoming message</p>
           </div>
         </div>
+
+        <div class="chat outgoing">
+          <div class="details">
+            <p>This is an outgoing message This is an outgoing messageThis is an outgoing message</p>
+          </div>
+        </div>
+        <div class="chat incoming">
+          <img src="#" alt="h">
+          <div class="details">
+            <p>This is an incoming message This is an incoming message This is an incoming message</p>
+          </div>
+        </div>
+        <div class="chat outgoing">
+          <div class="details">
+            <p>This is an outgoing message This is an outgoing messageThis is an outgoing message</p>
+          </div>
+        </div>
+        <div class="chat incoming">
+          <img src="#" alt="h">
+          <div class="details">
+            <p>This is an incoming message This is an incoming message This is an incoming message</p>
+          </div>
+        </div>
+        <div class="chat outgoing">
+          <div class="details">
+            <p>This is an outgoing message This is an outgoing messageThis is an outgoing message</p>
+          </div>
+        </div>
+        <div class="chat incoming">
+          <img src="#" alt="h">
+          <div class="details">
+            <p>This is an incoming message This is an incoming message This is an incoming message</p>
+          </div>
+        </div>
+
       </div>
+      <form action="#" class="typing-area">
+        <input type="text" placeholder="type a message here...">
+        <button><i class="fab fa-telegram-plane"></i></button>
+      </form>
    </section>
  </div>
 </body>
