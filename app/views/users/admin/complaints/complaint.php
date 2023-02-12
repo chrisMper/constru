26b25f10e7283860b58dd67db12c6fecf1e71957
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-02-13 00:39:20 +0530

    Add files via upload

--- app/views/users/admin/complaints/complaint.php
+++ app/views/users/admin/complaints/complaint.php
@@ -1,1 +1,64 @@
-dmw
+<!DOCTYPE html>
+<!-- <?php 
+include APPROOT. '/views/inc/navbar.php';
+?> -->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CV form</title>
+    <link rel="stylesheet" href="complaint.css">
+</head>
+
+<body>
+<div class="middle">
+    
+    <table class="carttbl">
+        <tr class="detail-border" >
+            <td c  width="100px" height="40px"> </td>
+            <td width="850px" height="40px"><center></center><input type="text" placeholder="Search Complaints" name="reason" required></center></td>
+            
+        </tr>
+        </table>
+    
+    <br>
+   <table style="border-radius: 50px;">
+    
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="profileicon.png" height="80px" width="80px"></td>
+        <td width="150px" height="40px">Person 1</td>
+        <td width="550px" height="40px">This is a text message</td>
+        <td width="150px" height="40px"><font color="red">2</font></td>
+    </tr>
+   </table>
+   <br>
+    <table style="border-radius: 50px;">
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="profileicon.png" height="80px" width="80px"></td>
+        <td width="150px" height="40px">Person 2</td>
+        <td width="550px" height="40px">This is a text message</td>
+        <td width="150px" height="40px"> </td>
+    </tr>
+    </table>
+    <br>
+    <table style="border-radius: 50px;">
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="profileicon.png" height="80px" width="80px"></td>
+        <td width="150px" height="40px">Person 3</td>
+        <td width="550px" height="40px">This is a text message</td>
+        <td width="150px" height="40px"><font color="red">1</font></td>
+    </tr>
+    </table>
+    <br>
+   <table style="border-radius: 50px;">
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="profileicon.png" height="80px" width="80px"></td>
+        <td width="150px" height="40px">Person 4</td>
+        <td width="550px" height="40px">This is a text message</td>
+        <td width="150px" height="40px"> </td>
+        
+    </tr>
+   </table>
+
+</div>
