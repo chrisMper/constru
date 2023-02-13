ae38da1a5f013fa4f71c6d209117b6bdc2baf818
chrisMper <chrisperera1999@gmail.com>
2023-02-13 12:16:42 +0530

    delete commit

--- app/views/inc/footer.php
+++
@@ -1,6 +0,0 @@
-  <!-- </div> -->
- 
-  <script src="<?php echo URLROOT; ?>/js/navbar.js"></script>
-  <script src="<?php echo URLROOT; ?>/js/index.js"></script>
-</body>
-</html>
--- app/views/inc/projectsnav.php
+++
@@ -1,4 +0,0 @@
-<a href="<?php echo URLROOT; ?>/pages/#"><button type="submit" class="navbtn">New</a>
-<a href="<?php echo URLROOT; ?>/pages/#"><button type="submit" class="navbtn">Ongoing</a>
-<a href="<?php echo URLROOT; ?>/pages/#"><button type="submit" class="navbtn">Completed</a>
-<a href="<?php echo URLROOT; ?>/pages/#"><button type="submit" class="navbtn">Cancelled</a>
