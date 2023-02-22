b7602d8a0dfa525b394b956c4993b18ee1b58700
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-02-23 00:57:54 +0530

    Add files via upload

---
+++ app/views/users/admin/joinreq/join_requests.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!-- <?php 
+include APPROOT. '/views/inc/navbar.php';
+?> -->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>complaints</title>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/join_requests.css">
+</head>
+
+<body>
+<body>
+<div class="middle">
+    
+    
+   <table style="border-radius: 50px">
+    
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="<?php echo URLROOT; ?>/img/profileicon.png" height="80px" width="80px"></td>
+        <td width="450px" height="40px">Service provider</td>
+        <td width="150px" height="40px"><div class="urgent">Urgent</div></td>
+        <td width="200px" height="40px"><button>View</button></td>
+    </tr>
+   </table>
+   <br>
+    <table style="border-radius: 50px;">
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="<?php echo URLROOT; ?>/img/profileicon.png" height="80px" width="80px"></td>
+        <td width="450px" height="40px">Service provider</td>
+        <td width="150px" height="40px"></td>
+        <td width="200px" height="40px"><button>View</button></td>
+    </tr>
+    </table>
+    <br>
+    <table style="border-radius: 50px;">
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="<?php echo URLROOT; ?>/img/profileicon.png" height="80px" width="80px"></td>
+        <td width="450px" height="40px">Service provider</td>
+        <td width="150px" height="40px"></td>
+        <td width="200px" height="40px"><button>View</button></td>
+    </tr>
+    </table>
+    <br>
+   <table style="border-radius: 50px;">
+   <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="<?php echo URLROOT; ?>/img/profileicon.png" height="80px" width="80px"></td>
+        <td width="450px" height="40px">Service provider</td>
+        <td width="150px" height="40px"></td>
+        <td width="200px" height="40px"><button>View</button></td>
+    </tr>
+   </table>
+
+</div>
+
+
+</body>
+</html>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
