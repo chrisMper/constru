a227e10188b10fd49c3f87bc8d3b7a7c16a55c37
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-03-06 23:49:43 +0530

    commit

---
+++ app/views/users/admin/joinreq/join_req_view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!-- <?php 
+include APPROOT. '/views/inc/admin_navbar.php';
+?> -->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>complaints</title>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/join_requests.css">
+</head>
+
+<body>
+
+<div class="details">
+
+</body>
+</html>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
