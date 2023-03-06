0884d1e34b86cf8cea3e946575ec25859591b656
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-03-06 23:56:23 +0530

    commit all SP

---
+++ app/views/users/admin/serviceproviders/comps.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!-- <?php 
+include APPROOT. '/views/inc/admin_navbar.php';
+?> -->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>complaints</title>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/comps.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+<div class="middle">
+    <div><h2></h2></div>
+    <div search>
+    <input type="text" placeholder="Search Customers here" name="reason" required>
+</div>
+ <div class="cus-tbl">
+    <table>
+         <tr>
+            <td  class="name">Profile</td>
+            <td   class="address">Company name</td>
+            <td class="phone">Phone no</td>
+            <td class="address">E-mail</td>
+            <td class="active-stts">Active status</td>
+        </tr>
+        <tr class="empty"> </tr>
+        <tr class="rows">
+            <td class="space">img</td>
+            <td class="space">mdm.pvt.ltd</td>
+            <td class="space">0771831005</td>
+            <td class="space">madu@gmail.com</td>
+            <td class="space">Active</td>
+        </tr>
+        <tr class="rows">
+            <td class="space">img</td>
+            <td class="space">My Home</td>
+            <td class="space">07728394595</td>
+            <td class="space">crisperera@gmail.com</td>
+            <td class="space">Deactive</td>
+        </tr>
+        <tr class="rows">
+            <td class="space">img</td>
+            <td class="space">abc.pvt.ltd</td>
+            <td class="space">0912264541</td>
+            <td class="space">tharindisilva@gmail.com</td>
+            <td class="space">Active</td>
+        </tr>
+
+
+         
+    </table>
+</div>
+       
+</div>
+</body>
+
