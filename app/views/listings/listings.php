d24666d205e19b1779348697611a30cf05408fb7
yasinthags <108751097+yasinthags@users.noreply.github.com>
2023-03-16 11:13:18 +0530

    Add files via upload

--- app/views/listings/listings.php
+++ app/views/listings/listings.php
@@ -1,41 +1,61 @@
+<html>
+<head>
+
+  <title>listings</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/supplierDashboard.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
 <body>
-<!------------------------------ navbar ------------------------------------>
-<div class='navbar' >
-<?php include APPROOT . '/views/inc/navbar.php'; ?>
-</div>
 
-<!-- -----------------------------list-container------------------------------- -->
-<main>
+<?php include APPROOT . '/views/inc/navbar_sup.php'; ?>
 
-<div class="addbtn-container">
-<a href="<?php echo URLROOT; ?>/listings/add">
-<button type="button" class="addbtn">Add</button>
-</a>
-</div>
+    <div class="main">
+      <button class="addbtn"><a href="<?php echo URLROOT; ?>/pages/addlistings">Add New</a></button>
+      <div class="listing">
+              <?php foreach($data['listings'] as $listings) : ?>
+              <div class='listing-card'>
+              <div>
+                <img src='<?php echo URLROOT; ?>/img/product_img/product_placeholder.png'> 
+              </div>
+              <div>
+                <h3><?php echo $listings->product; ?></h3>
+                <div>
+                  <div><label for=''>Price</label><input type='text' value='<?php echo $listings->price; ?>' readonly></div>
+                  <div><label for=''>Availability</label><input type='text' value='<?php echo $listings->availability; ?>' readonly></div>
+                  <div><label for=''>Available Quantity</label><input type='text' value='<?php echo $listings->quantity; ?>' readonly></div>
+                </div>
+              </div>
+              <div>
+                 <div></div><label for=''>Delivery within(approx)</label><input type='text' value='<?php echo $listings->time; ?>' readonly>
+              </div>
+              <div>
+                <?php if($data['listings']->user_id ==$_SESSION['user_id']):?>
+                
+                <div><button class="updatebtn"><a href="<?php echo URLROOT; ?>/pages/updatelistings/<?php echo $data['listings']->id;?>">UPDATE</a></button></div>
+                <div><form action="<?php echo URLROOT; ?>/listings/deletelistings/<?php echo $data['listings']->id;?>" method="POST">
+                     <button type="submit" class="deletebtn">DELETE</button>
+                </form></div>
 
-
-<?php foreach($data['listings'] as $listing) :  ?>
-    <?php if( ($listing->UserEmail)== $_SESSION['user_email']): ?>
-        
-<div class = "list-container">
-    <div class="left-mid">
-    <div class = "left-col">
-    <img src="<?php echo URLROOT; ?>/img/uploads/uploads<?php echo $listing->Photos; ?> ">
-    </div> 
-    <div class = 'mid-col'>
-        <p class='topic'><?php echo $listing->Title; ?> <p>
-        <p class='decription'><?php echo $listing->Description; ?><p>
+                <?php endif;?>
+              </div>
+            </div>
+            <?php endforeach; ?>
+      </div>
     </div>
-</div>
-    <div class = 'right-col'>
-    <button type="button" class="updatebtn">Update</button><br>
-    <button type="button" class="deletebtn">Delete</button>
-    </div>
-</div>
-<?php endif; ?> 
-<?php endforeach; ?>
 
+    <?php include APPROOT . '/views/inc/footer.php'; ?>
+  <!-- <div class="footer">
+      
+        <div class="footerleft">
+            <h5>Sri Lanka| English (US)</h5>
+        </div>
+        <div class=" footerright">
+            <h5>&copy; 2022 Constru+,inc.    Privacy   Terms of Use</h5>
+        </div>
+      
+    </div>  -->
 
+</body>
 
-</main>
-
+</html>
