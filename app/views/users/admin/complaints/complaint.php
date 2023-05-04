0839c5da67dabc55c20bb8b7706f2fe834e67603
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-05-05 00:25:04 +0530

    changed

--- app/views/users/admin/complaints/complaint.php
+++ app/views/users/admin/complaints/complaint.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <!-- <?php 
-include APPROOT. '/views/inc/navbar.php';
+include APPROOT. '/views/inc/admin_navbar.php';
 ?> -->
 
 <html lang="en">
@@ -14,7 +14,7 @@ include APPROOT. '/views/inc/navbar.php';
 <body>
 <div class="middle">
     
-    <table class="carttbl">
+    <!-- <table class="carttbl">
         <tr class="detail-border" >
             <td c  width="100px" height="40px"> </td>
             <td width="850px" height="40px"><center></center><input type="text" placeholder="Search Complaints" name="reason" required></center></td>
@@ -59,9 +59,22 @@ include APPROOT. '/views/inc/navbar.php';
         <td width="150px" height="40px"> </td>
         
     </tr>
-   </table>
-
+   </table> -->
+  
+   <table style="border-radius: 50px;">
+   <?php 
+        
+        foreach ($data['users'] as $user): ?>
+    <tr class="detail-border" >
+        <td class="item-border" width="100px" height="40px"><img src="<?php echo URLROOT; ?>/img/profileicon.png" height="80px" width="80px"></td>
+        <td width="150px" height="40px"><?php echo $user->role; ?></td>
+        <td width="550px" height="40px"><?php echo $user->type; ?></td>
+        <td width="150px" height="40px"><button>View</button></td>
+    </tr>
+    <?php endforeach; ?>
+    </table></br>
+    
 </div>
 </body>
-
+</html>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
