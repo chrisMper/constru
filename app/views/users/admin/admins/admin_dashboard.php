8540a8e2553f18840001c141ebcb97781651e78a
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-05-05 00:27:42 +0530

    commit

---
+++ app/views/users/admin/admins/admin_dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!-- <?php 
+include APPROOT. '/views/inc/admin_navbar.php';
+?> -->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>complaints</title>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/Admindash.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+
+      <div class="card">    
+        <table>
+          <tr>
+            <td><a href="<?php echo URLROOT; ?>/joinreq/join_requests.php"><img src="<?php echo URLROOT; ?>/img/design2.jpg"><br /><h4>Join Requests</h4></a></td>
+            <td><a href="bottle.html"><img src="<?php echo URLROOT; ?>/img/design3.jpg"><br /><h4>Complaints</h4></a></td>
+            <td><a href="watch.html"><img src="<?php echo URLROOT; ?>/img/design5.jpg"><br /><h4>Service providers</h4></a></td>
+          </tr>
+          <tr>
+            <td><a href="adminlist.php"><img src="<?php echo URLROOT; ?>/img/design7c.jpg"><br /><h4>Admins</h4></a></td>
+            <td><a href="phone.html"><img src="<?php echo URLROOT; ?>/img/design6.jpg"><br /><h4>Profile</h4></a></td>
+            <td><a href="phone.html"><img src="<?php echo URLROOT; ?>/img/design8.jpg"><br /><h4>Statistics</h4></a></td>
+          </tr>
+          
+        </table>
+      </div>
+  </div>
+
+
+</body>
