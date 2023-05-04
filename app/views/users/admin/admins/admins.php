135a850901ee61ce420a302bc061ec5fd34400f3
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-05-05 00:26:01 +0530

    updated

--- app/views/users/admin/admins/admins.php
+++ app/views/users/admin/admins/admins.php
@@ -19,27 +19,39 @@ include APPROOT. '/views/inc/admin_navbar.php';
             <div style="width:90%;">
                 <h1>Constru+</h1>
                 <h2 style="color:rgb(92, 90, 90)">Admins</h2>         
-                <div class="div4"><a href="add_admin.php"><button class="btn"><b>Add an Admin</b></button></a></div>
+                <div class="div4"><a href="<?php echo URLROOT; ?>/admins/add_admin"><button class="btn"><b>Add an Admin</b></button></a></div>
             </div>
 
 
+<!-- table -->
+<div class="middle">
+<table>
+    <thead>
+        <th>First Name</th>
+        <th>last Name</th>
+        <th>Contact Number</th>
+        <th>Email</th>
+        <th>NIC</th>
+        <th>Address</th>
+        <th>Password</th>
+    </thead>
 
-            <!-- table -->
-            <center>
-            <table>
-                <thead>
-                  <th>Name</th>
-                  <th>Contact Number</th>
-                  <th>Email</th>
-                  <th>NIC</th>
-                  <th>Address</th>
-                  <th>profile</th>
-
-                </thead>
-
-                
-            </table>
-            </center>
-
+    <tbody>
+        <?php 
+        
+        foreach ($data['users'] as $user): ?>
+        <tr >
+            <td><?php echo $user->fname; ?></td>
+            <td><?php echo $user->lname; ?></td>
+            <td><?php echo $user->contact_number; ?></td>
+            <td><?php echo $user->email; ?></td>
+            <td><?php echo $user->nic; ?></td>
+            <td><?php echo $user->address; ?></td>
+            <td><?php echo $user->password; ?></td>
+            <td ><a href="<?php echo URLROOT; ?>/admins/update_admin/<?php echo $user->id; ?>"><button>Update</button></a></br> <a href="<?php echo URLROOT; ?>/admin_list/deleteAdmin/<?php echo $user->email; ?>"><button>Delete</button></td>
+            <!-- <td ><button>Delete</button></td> -->
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
         </div>
-                    </body>
