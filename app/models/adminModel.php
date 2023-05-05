5e8106fae3200e74b0062fb6288c12d15039fe52
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-05-06 00:17:32 +0530

    New Updates

--- app/models/adminModel.php
+++ app/models/adminModel.php
@@ -62,5 +62,39 @@ class adminModel {
         return false;
       }
     }
+
+    public function deleteAdmin($email) {
+      $this->db->query('DELETE FROM admins WHERE email = :email');
+      $this->db->bind(':email', $email);
+      if($this->db->execute()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+
+    public function updateAdmin($data) {
+      // Update the data in the database
+      $this->db->query('UPDATE admins SET fname = :fname, lname = :lname, contact_number = :contact_number, email = :email, nic = :nic, age = :age, address = :address WHERE id = :id');
+      // Bind values
+      $this->db->bind(':id', $data['id']);
+      $this->db->bind(':fname', $data['fname']);
+      $this->db->bind(':lname', $data['lname']);
+      $this->db->bind(':contact_number', $data['contact_number']);
+      $this->db->bind(':email', $data['email']);
+      $this->db->bind(':nic', $data['nic']);
+      $this->db->bind(':age', $data['age']);
+      $this->db->bind(':address', $data['address']);
+      // Execute query
+      if($this->db->execute()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+
+
   }
   ?>
