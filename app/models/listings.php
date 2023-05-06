76ef9934422cff8cff9ff26d90bdbd4641c192de
yasinthags <108751097+yasinthags@users.noreply.github.com>
2023-05-06 13:52:44 +0530

    Update listings.php

--- app/models/listings.php
+++ app/models/listings.php
@@ -1,30 +1,32 @@
 <?php
 
-class listings{
+class item{
 
     private $db;
     public function __construct(){
         $this->db = new Database;
     }
 
-    public function getListings(){
-        $this->db->query('SELECT * , supplier.user_id as user_id, listing.id as listing_id FROM listing INNER JOIN supplier ON listing.id = supplier.user_id ORDER BY listing.created_at DESC');
+    public function getItems(){
+        $this->db->query('SELECT * FROM supplier_item ');
         $results = $this->db->resultSet();
         return $results;
     }
 
-    public function addlistings($data){
-        $this->db->query('INSERT INTO listings(product, description, price, time, availability, quantity, stockamount, product_img,user_id) VALUES (:product, :description, :price, :time, :availability, :quantity, :stockamount, :product_img,:user_id)');
+    public function addItems($data,$file){
+        $this->db->query('INSERT INTO supplier_item (item, description, price, code, quantityInStock, category, itemImage,email) VALUES (:item, :description, :price, :code, :quantityInStock, :category, :itemImage,:email)');
        
-        $this->db->bind(':user_id',$data['user_id']);
-        $this->db->bind(':product',$data['product']);
+
+        $this->db->bind(':email',$data['email']);
+        $this->db->bind(':item',$data['item']);
         $this->db->bind(':description',$data['description']);
-        $this->db->bind(': price',$data['price']);
-        $this->db->bind(':time',$data['time']);
-        $this->db->bind(':availability',$data['availability']);
-        $this->db->bind(':quantity',$data['quantity']);
-        $this->db->bind(':stockamount',$data['stockamount']);
-        $this->db->bind(':product_img',$data['product_img']);
+        $this->db->bind(':price',$data['price']);
+        $this->db->bind(':code',$data['code']);
+        $this->db->bind(':quantityInStock',$data['quantityInStock']);
+        $this->db->bind(':category',$data['category']);
+        $this->db->bind(':itemImage',$file['file_name']);
+
+        move_uploaded_file($file['tmp_name'],$file['upload_to'].$file['file_name']);
 
         if($this->db->execute()){
             return true;
@@ -33,18 +35,21 @@ class listings{
         }
     }
 
-    public function updatelistings($data){
-        $this->db->query('UPDATE listings SET product=:product, description=:description, price= :price, time= :time, availability= :availability, quantity= :quantity, stockamount= :stockamount, product_img= :product_img WHERE id=:id');
+    public function updateItems($data,$file){
+        
+        $this->db->query('UPDATE supplier_item SET item=:item, description=:description, price= :price, code= :code, quantityInStock= :quantityInStock, category= :category, itemImage= :itemImage, email= :email WHERE supplierItemId=:supplierItemId');
        
-        $this->db->bind(':id',$data['id']);
-        $this->db->bind(':product',$data['product']);
+        $this->db->bind(':supplierItemId',$data['supplierItemId']);
+        $this->db->bind(':item',$data['item']);
         $this->db->bind(':description',$data['description']);
-        $this->db->bind(': price',$data['price']);
-        $this->db->bind(':time',$data['time']);
-        $this->db->bind(':availability',$data['availability']);
-        $this->db->bind(':quantity',$data['quantity']);
-        $this->db->bind(':stockamount',$data['stockamount']);
-        $this->db->bind(':product_img',$data['product_img']);
+        $this->db->bind(':price',$data['price']);
+        $this->db->bind(':code',$data['code']);
+        $this->db->bind(':quantityInStock',$data['quantityInStock']);
+        $this->db->bind(':category',$data['category']);
+        $this->db->bind(':itemImage',$file['file_name']);
+        $this->db->bind(':email',$data['email']);
+
+        move_uploaded_file($file['tmp_name'],$file['upload_to'].$file['file_name']);
 
         if($this->db->execute()){
             return true;
@@ -53,15 +58,16 @@ class listings{
         }
     }
 
-    /*public function getListingById($id){
-        $this->db->query('SELECT * FROM listing WHERE id=:id');
-        $this->db->bind(':id',$id);
+    public function getItemById($supplierItemId){
+        $this->db->query('SELECT * FROM supplier_item WHERE supplierItemId=:supplierItemId');
+        $this->db->bind(':supplierItemId',$supplierItemId);
         $row=$this->db->single();
         return $row;
-    }*/
-    public function deletelistings($id) {
-        $this->db->query('DELETE FROM listing WHERE id = :id');
-        $this->db->bind(':id', $id);
+    }
+
+    public function deleteItems($supplierItemId) {
+        $this->db->query('DELETE FROM supplier_item WHERE supplierItemId = :supplierItemId');
+        $this->db->bind(':supplierItemId', $supplierItemId);
         
         if($this->db->execute()){
             return true;
