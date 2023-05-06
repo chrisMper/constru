17dbeb66ed331efd5650f6a06f33dfd0c0aadd19
yasinthags <108751097+yasinthags@users.noreply.github.com>
2023-05-06 13:42:35 +0530

    Update addlistings.php

--- app/views/users/sup/listings/addlistings.php
+++ app/views/users/sup/listings/addlistings.php
@@ -1,23 +1,25 @@
 <html>
 <head>
 
-  <title>listings</title>
+  <title>Items</title>
   <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/supplierDashboard.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body >
 
 <?php include APPROOT . '/views/inc/navbar_sup.php'; ?>
-<a href="<?php echo URLROOT; ?>/listings" ><i class='bx bx-arrow-back'></i>Back </a>
+   <div class="main">
+
+   <a href="<?php echo URLROOT; ?>/items/items" ><i class='bx bx-arrow-back'></i>Back </a>
       <div class="addform">
 
-      <form  method = "post" id="addform" action="<?php echo URLROOT; ?>/listings/addlistings">
+      <form  method = "post" id="addform" action="<?php echo URLROOT; ?>/items/addItems" enctype="multipart/form-data">
 
         <table  border="0">
          
           <tr>
-             <td>Product</td>
-             <td colspan="2"><input type="text" id="product" name="product" required></td>
+             <td>Item</td>
+             <td colspan="2"><input type="text" id="item" name="item" required></td>
              <td></td>
           </tr>
           <tr>
@@ -26,66 +28,47 @@
           <tr>
              <td colspan="4"><textarea name="description" class="textarea" required></textarea></td>
           </tr>
-          <tr>
+          <tr>          
+            <td>Code</td>
+            <td><input type="number" id="code" name="code" required></td>
             <td>Price (Rs.)</td>
             <td><input type="number" id="price" name="price" required></td>
-            <td>Delivery within (approx)</td>
-            <td><input type="text" id="time" name="time" required></td>
           </tr>
           <tr>
-            <td>Availability</td>
-            <td><input type="text" id="availability" name="availability" required></td>
             <td>Available quantity</td>
-            <td><input type="number" placeholder="Enter stock amount" id="quantity" name="quantity" required></td>
-          </tr>
-          <tr>
+            <td><input type="number" placeholder="Enter stock amount" id="quantityInStock" name="quantityInStock" required></td>
             <td>Category</td>
-            <td><select required>
-              <option>Building materials</option>
-              <option>Tools sets</option>
-              <option>Electrical supplies</option>
-              <option>Plumbing supplies</option>
-              <option>Housewares</option>
-              <option>Garden products</option>
-              <option>Cleaning supplies</option>
+            <td><select name="category" required>
+              <option value="">Select the category</option>
+              <option value="Building materials">Building materials</option>
+              <option value="Tools sets">Tools sets</option>
+              <option value="Electrical supplies">Electrical supplies</option>
+              <option value="Plumbing supplies">Plumbing supplies</option>
+              <option value="Housewares">Housewares</option>
+              <option value="Garden products">Garden products</option>
+              <option value="Cleaning supplies">Cleaning supplies</option>
             </select></td>
-          </tr>
-          <tr>
-            <td colspan="3"><input type="checkbox" id="check" name="check" value="check" >       I would like to receive a message when stock is less than </td>
-            <td></td>
-            <td></td>
           </tr>
           <tr>
              <td colspan="4">Upload pictures</td>
           </tr>
           <tr>
-             <td><input type="file" id="product_img" name="product_img" required></td>
+             <td><input type="file" id="itemImage" name="itemImage" required></td>
           </tr>
           
 
         </table>
         <div class="form-actions">
-        <button type="submit" class="submitbtn">Submit</button>
-        <button  class="discardbtn"><a href="<?php echo URLROOT; ?>/pages/listings/listings">Discard </a></button>
+          <button type="submit" class="submitbtn">Submit</button>
+          <button  class="discardbtn"><a href="<?php echo URLROOT; ?>/items/items/items">Discard </a></button>
         </div>
         
       </form>
       
     </div>
 
-  
 
-
-    <div class="footer">
-      
-        <div class="footerleft">
-            <h5>Sri Lanka| English (US)</h5>
-        </div>
-        <div class=" footerright">
-            <h5>&copy; 2022 Constru+,inc.    Privacy   Terms of Use</h5>
-        </div>
-      
+    <?php include APPROOT . '/views/inc/footer.php'; ?>
     </div>
-
   </body>
 </html>
