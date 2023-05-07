ea950b07fdd186e3e3d6242cf986f68073a1f32a
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-05-08 00:29:52 +0530

    Update customerModel.php

--- app/models/customerModel.php
+++ app/models/customerModel.php
@@ -19,24 +19,6 @@ class customerModel {
       return $customers;
   }
   
-    // public function addCustomer($data) {
-    //   // Insert the data into the database
-    //   $this->db->query('INSERT INTO customers (fname, lname, contact_number, email, nic, age, address, profile_image) VALUES (:fname, :lname, :contact_number, :email, :nic, :age, :address, :profile_image)');
-    //   // Bind values
-    //   $this->db->bind(':first_name', $data['fname']);
-    //   $this->db->bind(':last_name', $data['lname']);
-    //   $this->db->bind(':contact', $data['contact_number']);
-    //   $this->db->bind(':email', $data['email']);
-    //   $this->db->bind(':nic', $data['nic']);
-    //   $this->db->bind(':age', $data['age']);
-    //   $this->db->bind(':address', $data['address']);
-    //   $this->db->bind(':profile_image', $data['profile_image']);
-    //   // Execute query
-    //   if($this->db->execute()) {
-    //     return true;
-    //   } else {
-    //     return false;
-    //   }
-    // }
+   
   }
   ?>
