4002ba298545483ffcc96142f8b14a7b215c7bb1
madushatharindi <108750276+madushatharindi@users.noreply.github.com>
2023-05-08 00:28:07 +0530

    updated_sp_controller

--- app/controllers/sp_list.php
+++ app/controllers/sp_list.php
@@ -9,67 +9,32 @@ class sp_list extends Controller {
     }
 
 
-    public function index(){
-      //Get Post
-      $engs=$this->spModel->getEngs();
-
-      // $data = [
-      //     'admins'=>$admins
-      // ];
-      
-      // $this->view('users/admin/admins', $data);
-      include 'views/engs.php';
-    }
-  
-
-    
-  }
+    public function deleteEng($email) {
+      if($this->spModel->deleteEng($email)) {
+        redirect('serviceproviders/engs');
+      } else {
+        die('Something went wrong.');
+      }
+    } 
 
 
-  class sp_list extends Controller {
-    public function __construct() {
-      // if(!isset($_SESSION['user_email'])){
-      //   redirect('users/login');
-      // }
-      $this->spModel = $this->model('spModel');
-    }
+    public function deleteSup($email) {
+      if($this->spModel->deleteSup($email)) {
+        redirect('serviceproviders/sups');
+      } else {
+        die('Something went wrong.');
+      }
+    } 
 
+    public function deleteComp($email) {
+      if($this->spModel->deleteComp($email)) {
+        redirect('serviceproviders/comps');
+      } else {
+        die('Something went wrong.');
+      }
+    } 
 
-    public function index(){
-      //Get Post
-      $sups=$this->spModel->getSups();
-
-      //
-      // $this->view('users/admin/admins', $data);
-      include 'views/sups.php';
-    }
-  
-
-    
-  }
-
-  class sp_list extends Controller {
-    public function __construct() {
-      // if(!isset($_SESSION['user_email'])){
-      //   redirect('users/login');
-      // }
-      $this->spModel = $this->model('spModel');
-    }
-
-
-    public function index(){
-      //Get Post
-      $comps=$this->spModel->getComps();
-
-      // $data = [
-      //     'admins'=>$admins
-      // ];
-      
-      // $this->view('users/admin/admins', $data);
-      include 'views/comps.php';
-    }
-  
-
+ 
     
   }
   ?>
